6f34e59866c59181e1a67b35486dfebf5107cb81
cxalem <smarketing.cuz@gmail.com>
2022-01-05 22:23:51 -0500

    Lógica del juego lista

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function showPlayground() {    
     
     let okForm = true;
@@ -18,9 +16,9 @@ function showPlayground() {
         const element = document.getElementById('namedTitle');
         element.insertAdjacentElement("afterbegin", title);
 
-        let showDisplay = document.getElementById('playgroundContainer');
-        let hideDisplay = document.getElementById('playerName');
-        let containerDisplay = document.getElementById('container');
+        const showDisplay = document.getElementById('playgroundContainer');
+        const hideDisplay = document.getElementById('playerName');
+        const containerDisplay = document.getElementById('container');
         showDisplay.style.display = 'flex';
         hideDisplay.style.display = 'none';
         containerDisplay.style.display = 'block';
@@ -28,4 +26,36 @@ function showPlayground() {
     }
  }
 
- 
+
+const piedra = document.getElementById('rock');
+const papel = document.getElementById('paper');
+const tijeras = document.getElementById('scissors');
+const options = [piedra, papel, tijeras];
+
+function getCpuChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    return options[randomNumber];
+  }
+
+options.map( option =>{
+    option.addEventListener('click', e => {
+        if (e.target == piedra) {
+            const cpuChoice = getCpuChoice();
+            if (cpuChoice == papel) {
+                console.log('perdiste');
+            }
+            else if (cpuChoice == tijeras) {
+                console.log('ganaste');
+            }
+            else if(cpuChoice == piedra) {
+                console.log('empate');
+            }
+        }
+        else if (e.target == tijeras) {
+            const cpuChoice = getCpuChoice();
+            if (cpuChoice == piedra) {
+                console.log('perdiste');
+            }
+        }
+    })
+})
